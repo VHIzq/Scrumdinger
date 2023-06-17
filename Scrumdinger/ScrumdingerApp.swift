82d6dde33f61820866c522a91304f6348cf9271b
vhmi <vhmizq@gmail.com>
2023-06-17 13:22:17 -0600

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by VÍCTOR HUGO IZQUIERDO on 17/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
