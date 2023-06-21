ab134c8b02b0d923f1a3cf6ce889f2aee7330090
vhmi <vhmizq@gmail.com>
2023-06-20 20:57:45 -0600

    add new View Details Screen

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,54 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by VÍCTOR HUGO IZQUIERDO on 20/06/23.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    @State private var newAttendeeName = " "
+    @State private var scrum = DailyScrum.emptyScrum
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info") ) {
+                TextField("Title", text: $scrum.title)
+                HStack {
+                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1){
+                        Text("Length")
+                    }
+                    Spacer( )
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indices in
+                    scrum.attendees.remove(atOffsets: indices)
+                }
+                HStack{
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(action: {
+                        withAnimation {
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                            scrum.attendees.append(attendee)
+                            newAttendeeName = " "
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    .disabled(newAttendeeName.isEmpty)
+                }
+            }
+        }
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
